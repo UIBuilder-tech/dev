a1bb7281573a9d1b60aac5ec70b93ceb607473a2
UIBuilder <anirudha2001.alt@gmail.com>
2024-12-21 23:56:43 +0530

    updated vit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from '@svgr/rollup';
+import image from '@rollup/plugin-image';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     svgr({
       exportAsDefault: false,
-    }),],
+    }),
+    image()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
