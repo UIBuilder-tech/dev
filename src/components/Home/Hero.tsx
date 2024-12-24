5ec81d9aa858d6e616d4a919a91308836d2df8f0
UI-builder2024 <ui.builder22@gmail.com>
2024-12-24 06:30:05 +0530

    updated hero section styles for feedback

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero({ title, desc, img, from = "home" }: Props) {
         <div className="absolute inset-0 bg-black/50"></div>
       </div>
 
-      <div className="relative max-w-7xl mx-auto px-4 md:pt-32 pt-24 max-sm:pb-16">
+      <div className="relative max-w-7xl mx-auto ml-10 md:pt-32 pt-24 max-sm:pb-16">
         <div className="max-w-3xl pt-10 max-sm:pb-10 md:pt-15">
           <h1 className="font-display text-3xl md:text-5xl text-white mb-3 md:mb-6 leading-tight">
             {/* {from === "home"
@@ -33,7 +33,7 @@ export default function Hero({ title, desc, img, from = "home" }: Props) {
               </p>
             )}
           </h1>
-          <p className="text-white/90 text-sm md:text-lg mb-8 leading-relaxed max-sm:leading-2">
+          <p className="text-white/90 text-sm md:text-[18px] mb-8 leading-relaxed max-sm:leading-2">
             {/* {from === "home"
               ? "Founded in 2005, the Chitrapur Heritage Foundation (CHF) is a nonprofit organization dedicated to fostering sustainable development and preserving the rich cultural heritage of the Chitrapur Saraswat community. With a focus on Heritage, Education,Women's Empowerment and Spiritual Development"
               : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet."}
