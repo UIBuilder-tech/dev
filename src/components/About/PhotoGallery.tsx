6a7f84702141824b0473035c0a13b2b1933f4385
UI-builder2024 <ui.builder22@gmail.com>
2025-01-20 22:35:20 +0530

    `Refactor PhotoGallery component: update API request, add image splitting and minor code adjustments`

--- src/components/About/PhotoGallery.tsx
+++ src/components/About/PhotoGallery.tsx
@@ -6,32 +6,34 @@ const AdminPanelUrl = import.meta.env.VITE_ADMIN_PANEL_API;
 function PhotoGallery() {
   const triggerImagePreview = useImagePreviewTrigger();
   const [PageData, setPageData] = useState([]);
-      const { setData } = UseDataContext();
+  const { setData } = UseDataContext();
   useEffect(() => {
     const api = async () => {
-          setData(v => ({ ...v, isLoading: true }))
+      setData((v) => ({ ...v, isLoading: true }));
       const requestOptions: any = {
-        method: 'GET',
-        redirect: 'follow'
+        method: "GET",
+        redirect: "follow",
       };
       fetch(`${AdminPanelUrl}/gallery-section?populate=*`, requestOptions)
-        .then(response => response.json())
-        .then(result => {
+        .then((response) => response.json())
+        .then((result) => {
           if (result?.data.image) {
             const images = result.data.image.map((img) => {
-              return {image:AdminPanelUrl.replace("/api", "") +img.url}
-            })
+              return { image: AdminPanelUrl.replace("/api", "") + img.url };
+            });
             setPageData(images);
           }
         })
-        .catch(error => console.log('error', error))
+        .catch((error) => console.log("error", error))
         .finally(() => {
-          setData(v => ({ ...v, isLoading: false }))
-        });;
-    }
+          setData((v) => ({ ...v, isLoading: false }));
+        });
+    };
 
     api();
-  }, [])
+  }, []);
+  const firstHalf = PageData.slice(0, Math.ceil(PageData.length / 2));
+  const secondHalf = PageData.slice(Math.ceil(PageData.length / 2));
   return (
     <div className="mx-auto pb-10 md:py-16">
       <div className="text-center">
@@ -39,7 +41,7 @@ function PhotoGallery() {
           Photo Gallery
         </h2>{" "}
         <Marquee pauseOnHover className="[--duration:20s]">
-          {PageData.map((review, index) => (
+          {firstHalf.map((review, index) => (
             <img
               key={index}
               className="relative md:h-[300px] desktop-1500:h-[275px] desktop-1500:w-[400px] h-[200px] w-[300px] md:w-[450px] cursor-pointer overflow-hidden rounded-[30px] flex flex-col justify-end object-cover bg-white desktop-1200:h-[225px] desktop-1200:w-[375px]"
@@ -49,7 +51,7 @@ function PhotoGallery() {
           ))}
         </Marquee>
         <Marquee reverse pauseOnHover className="[--duration:20s]">
-          {PageData.map((review, index) => (
+          {secondHalf.map((review, index) => (
             <img
               key={index}
               className="relative md:h-[300px]  desktop-1500:h-[275px] desktop-1500:w-[400px]  desktop-1200:h-[225px] desktop-1200:w-[375px] h-[200px] w-[300px] md:w-[450px] cursor-pointer overflow-hidden rounded-[30px] flex flex-col justify-end object-cover bg-white"
